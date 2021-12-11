1475517dc6c92a023d6fbd22e55f5e8218c67bc1
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2021-12-11 20:52:41 +0000

    Update Student.cpp

--- Student/Student.cpp
+++ Student/Student.cpp
@@ -6,96 +6,88 @@
  * Created on 12 October 2021, 16:19
  */
 
+
+/*
+ * File:   main.cpp
+ * Author: arwaalsubhi
+ *
+ * Created on 12 October 2021, 16:19
+ */
+
 #include <cstdlib>
 #include<iostream>
 #include<string>
 #include "TypestateLibrary.h"
+
+
 
 using namespace std;
 
 
 
 class Student {
-    
-
 public:
-    
-  string Name;
+    Student()=default;
+string Name;
 string studentID;
-
-
-
-Student(string n, string id){
-     Name=n;
     
-     studentID =id;
+void Register(){
+  cout<<Name<< " has been registered with ID" <<studentID<<endl;
+   }
+void Enrol(){
+  cout<<Name<< "has been enrolled in a course" << endl;
 }
-
-void REG(){
-   
-    
-  cout<<Name<< " has been registered with ID : " << studentID<<endl;
-   
-
-}
-void ENR(){
-    
-     
-  cout<<Name<< " has been enrolled in courses" << endl;
- 
-}
-
-void END(){
-   
-  cout<<Name<< " has been logged out" << endl;
-  
- 
+void LogOut(){
+   cout<<Name<<"has Logged out"<<endl;
   }
-
-
 };
-   
 
 
-
- enum class Studentstate{
-     
-    UNAPPROVED,
-    APPROVED,
-    ENROLLED,
-    END,
+enum class Studentstate{
+   UNAPPROVED,
+   APPROVED,
+   ENROLLED,
+   END,
 };
+
+
+
+
 
 using TypestateTool::map_transition;
-using TypestateTool::map_Repeated_transition;
 using TypestateTool::map_protocol;
-using TypestateTool::assign_to_class;
 
 
- // defined the protocol
+
+// define the protocol
  
-using Student_protocol=  map_protocol<map_transition<Studentstate::UNAPPROVED, Studentstate::APPROVED, &Student::REG>,map_Repeated_transition<Studentstate::APPROVED,Studentstate::ENROLLED, &Student::ENR>
-, map_transition<Studentstate::ENROLLED, Studentstate::END, &Student::END>>;
-// assign it to class
+using Student_protocol=  map_protocol<
+map_transition<Studentstate::UNAPPROVED, Studentstate::APPROVED, &Student::Register>,
+map_transition<Studentstate::APPROVED,Studentstate::ENROLLED, &Student::Enrol>,
+map_transition<Studentstate::ENROLLED,Studentstate::ENROLLED, &Student::Enrol>,
+ map_transition<Studentstate::ENROLLED, Studentstate::END, &Student::LogOut> >;
+
+Assign_to_Class(Student, Student_protocol);
 
 
- using ID= assign_to_class<Student, Student_protocol>;
- 
+
+
+
 int main(int argc, char** argv) {
+    Student s ;
     
-    Student s= Student("Arwa", "2598556a");
-
     
-    s.REG();
- 
-    s.ENR();
-    
-
-     s.END();
-     
-    
+    s.Register();
+    s.Enrol();
+    s.Enrol();
+    s.LogOut();
     return 0;
-}
+};
+
+
+
+
+
 
 
    
