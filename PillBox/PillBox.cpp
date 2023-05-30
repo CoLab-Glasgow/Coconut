9f82ea8603d7d31efd435fba36aa19375c1693ef
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-05-30 23:01:30 +0100

    Update PillBox.cpp

--- PillBox/PillBox.cpp
+++ PillBox/PillBox.cpp
@@ -13,13 +13,13 @@ using TypestateLibrary::Typestate_Template;
 
 
 class Drawer {
-public :
+public:
 
     Drawer(std::string pillName, int hour, int minute)
         : pillName_(pillName), hour_(hour), minute_(minute) {}
 
     std::string get_pill_name() {
-        return this->pillName_; 
+        return this->pillName_;
     }
     int get_the_hour() {
         return this->hour_;
@@ -40,13 +40,13 @@ private:
 class PillBox {
 
 public:
-    void activate_pillBox() {
+    void Activate_pillBox() {
         std::cout << "PillBox is active now! " << std::endl;
 
     }
     void addDrawers(Drawer* d) {
 
-       DrawersBox.push_back(*d);
+        DrawersBox.push_back(*d);
     }
 
     Drawer Process_System_Time(int h, int m) {
@@ -57,7 +57,7 @@ public:
         }
 
     }
- 
+
     void Deactivate_Pill_Box() {
 
     }
@@ -71,11 +71,11 @@ private:
 class RedLed {
 
 public:
-    
-    void Swich_ON(Drawer* d) {
-        std::cout << "It's time to take the" << d->get_pill_name() << "! " << "\n";
+
+    void Switch_ON(Drawer* d) {
+        std::cout << "It's time to take the" <<d->get_pill_name();
     }
-    void Swich_OFF(Drawer* d) {
+    void Switch_OFF(Drawer* d) {
         std::cout << "Drawer that has the pill " << d->get_pill_name() << " is now Closed!"
             << std::endl;
     }
@@ -90,49 +90,49 @@ public:
 
 
 
-BETTER_ENUM(domain , int, 
-    Idle, Active,NonActive, Pill_Time_On,RedLedON, RedLedOFF, RedLedBlinkinning)
-    
+BETTER_ENUM(domain, int,
+    Idle, Active, NonActive, Pill_Time_On, RedLedON, RedLedOFF, RedLedBlinkinning)
 
- using PillBox_typestate = Typestate_Template<
-    State<domain::Idle, &PillBox::activate_pillBox, domain::Active>,
+
+    using PillBox_typestate = Typestate_Template<
+    State<domain::Idle, &PillBox::Activate_pillBox, domain::Active>,
     State<domain::Active, &PillBox::Process_System_Time, domain::Pill_Time_On>,
-    State<domain::Pill_Time_On, &RedLed::Swich_ON, domain::RedLedON>,
-    State<domain::RedLedON, &RedLed::Swich_ON, domain::RedLedON>,
+    State<domain::Pill_Time_On, &RedLed::Switch_ON, domain::RedLedON>,
+    State<domain::RedLedON, &RedLed::Switch_ON, domain::RedLedON>,
     State<domain::RedLedON, &RedLed::Blink, domain::RedLedBlinkinning>,
-    State<domain::RedLedBlinkinning, &RedLed::Swich_OFF, domain::RedLedOFF>,
+    State<domain::RedLedBlinkinning, &RedLed::Switch_OFF, domain::RedLedOFF>,
     State<domain::Active, &PillBox::Deactivate_Pill_Box, domain::NonActive>
- > ;
+    >;
 
 using PillBox_ = TypestateClassConnector<PillBox, PillBox_typestate>;
-using RedLed_  = TypestateClassConnector<RedLed, PillBox_typestate>;
+using RedLed_ = TypestateClassConnector<RedLed, PillBox_typestate>;
 
 int main() {
 
-   
+
     Drawer drawer1("Panadol", 3, 50);
     Drawer drawer2("Piriton Antihistamine", 8, 40);
-   
-
-   auto h = 10;
-   auto min = 30; 
 
 
-   PillBox_ pillbox;
-   RedLed_ redled;
-  
-   (pillbox->* & PillBox::addDrawers)(&drawer1);
-   (pillbox->* & PillBox::addDrawers)(&drawer2);
+    auto h = 10;
+    auto min = 30;
 
-  (pillbox->* & PillBox::activate_pillBox)();
-   Drawer d = (pillbox->* &PillBox::Process_System_Time)(std::move(h), std::move(min));
-  (redled->* &RedLed::Swich_ON)(&d);
-  (redled->* &RedLed::Blink)(&d);
-  (redled->* &RedLed::Swich_OFF)(&d);
 
-   
-  
-   
+    PillBox_ pillbox1 , pillbox2;
+    RedLed_ redled;
+
+    (pillbox1->*&PillBox::addDrawers)(&drawer1);
+    (pillbox2->*&PillBox::addDrawers)(&drawer2);
+
+    (pillbox1->*& PillBox::Activate_pillBox)();
+   Drawer d = (pillbox->* & PillBox::Process_System_Time)(&h, &min);
+   (redled->* & RedLed::Switch_ON)(&d);
+   (redled->* & RedLed::Blink)(&d);
+  (redled->* & RedLed::Switch_OFF)(&d);
+
+
+
+
 
 
     return 0;
