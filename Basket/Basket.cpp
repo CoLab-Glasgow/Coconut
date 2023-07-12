78ba4a966622734b27e299eb90481d732deb6980
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-07-12 14:51:51 +0100

    Update Basket.cpp

--- Basket/Basket.cpp
+++ Basket/Basket.cpp
@@ -1,83 +1,66 @@
 
-#include "Basket_Class.h"
-//
-//  Basket.cpp
-//  shoppingBasket
-//
-//  Created by Arwa Alsubhi on 22/11/2021.
-//
-
-
-
-
 
 #include <vector>
 #include <algorithm>
 #include<string>
-#include "Cocount\TypestateLibrary.h""
+#include "Cocount\TypestateLibrary.h"
+
 using TypestateLibrary::TypestateClassConnector;
 using TypestateLibrary::State;
 using TypestateLibrary::Typestate_Template;
 
-// create a Basket class
+//Create a Basket class
 class Basket_Class {
 
-    // this Basket use a vector 
-    std::vector<std::string> items;
-    std::vector<float>  prices;
-    float total = 0;
+// this Basket uses a vector 
+std::vector<std::string> items;
+std::vector<float>  prices;
+float total = 0;
 
 public:
-    Basket_Class() {
+ Basket_Class() {
 
     }
 
-    // clear the vector
-    void clear() {
+    //Clear the vector
+ void clear() {
         items.clear();
         prices.clear();
         total = 0;
     }
 
-    // add items to the vector
-    void addItemsToBasket(std::string item, float price) {
+    //Add items to the vector
+void addItemsToBasket(std::string item, float price) {
 
         items.push_back(item);
-
-        prices.push_back(price);
+    prices.push_back(price);
 
     }
     // delete items from the vector
-    void deleteItem(std::string item, float price) {
+void deleteItem(std::string item, float price) {
         items.erase(std::remove(items.begin(), items.end(), item), items.end());
         prices.erase(std::remove(prices.begin(), prices.end(), price), prices.end());
-
-
     }
 
     // pay 
-    void calculate() {
+void calculate() {
 
-        for (float i : prices)
-            total = total + i;
-
-        std::cout << "Total price for the order = " << total;
-        items.clear();
-        prices.clear();
-        total = 0;
+for (float i : prices)
+    total = total + i;
+    std::cout << "Total price for the order = " << total;
+    items.clear();
+    prices.clear();
+     total = 0;
     }
 
-    // checking the basket for emptyness 
-    bool checkBasket() {
-        int i = 0;
-        for (; i < items.size(); i++) {
-
-
+    // checking the basket for emptiness 
+bool checkBasket() {
+    int i = 0;
+    for (; i < items.size(); i++) {
         }
-        if (i > 0) {
-            return true;
+    if (i > 0) {
+    return true;
         }
-
         return false;
     }
 
@@ -85,16 +68,16 @@ public:
 
 };
 
-// define states of the protocol 
+//Define states of the protocol 
 BETTER_ENUM(BasketState , int,
     EMPTY,
     NONEMPTY,
-    UNKOWN,
+    UNKNOWN,
     END
 )
 
 
-// define protocol
+//Define protocol
 using Basket_protocol = Typestate_Template<
     State<BasketState::EMPTY, &Basket_Class::addItemsToBasket, BasketState::NONEMPTY>,
     State<BasketState::EMPTY, &Basket_Class::clear, BasketState::END>,
@@ -106,16 +89,12 @@ using Basket_protocol = Typestate_Template<
 >;
 
 
-
-
 using Basket = TypestateClassConnector<Basket_Class, Basket_protocol>;
 
 int main(int argc, const char* argv[]) {
 
-
-    // insert code here...
-
     Basket basket1;
+    
     (basket1->*&Basket_Class::addItemsToBasket)("Book", 10.30);
     (basket1->*&Basket_Class::addItemsToBasket)("pen", 2.30);
     (basket1->*&Basket_Class::addItemsToBasket)("box", 5.0);
