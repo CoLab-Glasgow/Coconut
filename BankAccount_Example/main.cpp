438da58eae68af4a3a700056f68ee16db0870b15
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-02 11:40:38 +0100

    Update and rename BankAccount.cpp to main.cpp

---
+++ BankAccount_Example/main.cpp
@@ -0,0 +1,16 @@
+
+#include "SalaryManager.h"
+
+int main() {
+
+Account account;
+Manager manager;
+Storge storage; 
+   
+
+(storage->*&DataStorage::store)();
+(manager->*&SalaryManager::addSalary)(5000.00);
+      
+ return 0;
+    
+}
