5dd3a02fef2c5d19f485a6c7aae39b34ab0ade7f
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-17 13:40:02 +0100

    Update Vehicle.h

--- Inheritance/Case 3/Vehicle.h
+++ Inheritance/Case 3/Vehicle.h
@@ -1,19 +1,14 @@
 
 #include <iostream>
 
-class Vehicle
-{
-	
-
+class Vehicle {
 public:
-	void  Play_music() {
-		std::cout << "Music is On! " << std::endl;
+
+  void  Play_music() {
+	std::cout << "Music is On! " << std::endl;
 	}
 
-
-	void Adjust_Seats() {
+  void Adjust_Seats() {
 		std::cout << "Seats are adjusted! " << std::endl;
 	}
-
-
 };
