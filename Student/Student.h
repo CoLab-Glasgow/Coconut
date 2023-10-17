abbbeb90c4d82a89169465a555f1e9544acc2f5e
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-17 14:16:22 +0000

    Update Student.h

---
+++ Student/Student.h
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include<iostream>
+#include<string>
+#include "TypestateLibrary.h"
+
+using TypestateLibrary::TypestateClassConnector;
+using TypestateLibrary::State;
+using TypestateLibrary::Typestate_Template;
+
+class Student {
+public:
+    Student() { }
+    void Register(std::string n , std::string i) {
+        std::cout << n << " has been registered with ID" << i << std::endl;
+    }
+    void Enrol(std::string n) {
+        std::cout << n << "has been enrolled in a course" << std::endl;
+    }
+    void LogOut(std::string n) {
+        std::cout << n << "has Logged out" << std::endl;
+    }    
+
+};
+
+
+BETTER_ENUM (Studentstate, int, 
+    UNAPPROVED,
+    APPROVED,
+    ENROLLED,
+    END
+    )
+
+
+using StudentProtocol = Typestate_Template<
+    State<Studentstate::UNAPPROVED, &Student::Register, Studentstate::APPROVED>,
+    State<Studentstate::APPROVED, &Student::Enrol, Studentstate::ENROLLED>,
+    State<Studentstate::ENROLLED, &Student::Enrol, Studentstate::ENROLLED>,
+    State<Studentstate::END, &Student::LogOut, Studentstate::END>
+>;
+
+using student = TypestateClassConnector<Student, StudentProtocol>;
