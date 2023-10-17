129207bb0e3aca971c0bead70f287266902c57cb
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-17 13:36:32 +0000

    Update main.cpp

--- Inheritance/Case_4/main.cpp
+++ Inheritance/Case_4/main.cpp
@@ -1,11 +1,21 @@
+
 #include "Vehicle.h"
 #include "Car.h"
 #include "TypestateLibrary.h"
 
-
+static void operateVehicle (Vehicle& v) {
+    (v->*&Vehicle::Start)();
+    (v->*&Vehicle::Set_Speed)(70);
+    (v->*&Vehicle::Stop)();
+}
 
 int main() {
-	
-	
-	return 0;
-}
+    Car car;
+    Vehicle vehicle;
+    operateVehicle(vehicle);
+    operateVehicle(car);
+    (car->*&Car::Activate_cruise)(50);
+    (car->*&Car::Apply_brakes)(20);
+    (car->*&Car::Halt)();
+    return 0;
+ }
