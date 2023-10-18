30aad0ef95e63e5984d7a69be78a3d8dd071c655
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-18 13:50:08 +0100

    Update Vehicle.h

--- Inheritance/Case_1/Vehicle.h
+++ Inheritance/Case_1/Vehicle.h
@@ -2,12 +2,9 @@
 #ifndef Vehicle_HEADER
 #define Vehicle_HEADER
 // case 1 A class without typestate inherits from a class with typestate. 
-class Vehicle
-{
+class Vehicle{
 	long speed;
-
 public:
-
 	void Start() {
 		std::cout << "Starting the Vehicle" << std::endl;
 	}
@@ -15,7 +12,6 @@ public:
 		std::cout << "Vehicle speed is: " << s << std::endl;;
 		this->speed = s;
 	}
-
 	void Stop() {
 		std::cout << "Vehicle Stopped !";
 	}
