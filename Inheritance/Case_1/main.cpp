4ae475103086fbc893e2599e5081ba08a959c138
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-18 13:47:54 +0100

    Update main.cpp

--- Inheritance/Case_1/main.cpp
+++ Inheritance/Case_1/main.cpp
@@ -1,19 +1,11 @@
-
 #include "Car.h"
 #include "Vehicle.h"
-
-int main() {
-	 
-	 Vehicle V1; 
-	 Car C;
-     V1.Start();
-	 V1.Set_Speed(70);
-
-	C.Start();
-	C.Play_Music();
-
-	
-
-
-	 return 0;
- }
+int main() {	 
+Vehicle V1; 
+Car C;
+V1.Start();
+V1.Set_Speed(70);
+C.Start();
+C.Play_Music();
+return 0;
+}
