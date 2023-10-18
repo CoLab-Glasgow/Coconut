15b4d700cefe27b51f8337ef7eed6f1e7f58c187
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-10-18 13:48:32 +0100

    Update Car.h

--- Inheritance/Case_1/Car.h
+++ Inheritance/Case_1/Car.h
@@ -7,22 +7,13 @@ class Car : public Vehicle {
 	long speed;
 
 public:
-	
-
 	void  Play_Music(){
 		std::cout << "Music is On! " << std::endl;
 	}
-
-
 	void Adjust_Seats() {
 		std::cout << "Seats are adjusted! " << std::endl;
 	}
 	void Halt() {
 		std::cout << "The car is stopped" << std::endl;
-
 	}
-
-
-
-
 };
