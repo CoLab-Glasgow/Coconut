d159a889f95358d9c9432755a158171f3d8f07af
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2023-11-09 00:00:11 +0000

    Update main.cpp

--- PillBox/main.cpp
+++ PillBox/main.cpp
@@ -2,26 +2,17 @@
 
 #include<iostream>
 #include "PillBox.h"
-
 int main() {
-
-    PillBox pill;
-    
-
-    Drawer drawer1("Panadol", 3, 50);
-    Drawer drawer2("Piriton Antihistamine", 8, 40);
-    int h = 10;
-    int min = 30;
+    Drawer* drawer1 = new Drawer("Panadol", 3, 50);
+    Drawer* drawer2 = new Drawer("Piriton Antihistamine", 8, 40);
     Pillbox pillbox1;
     Pillbox& ptr_pillbox1 = pillbox1;
-   
-    //PillBox().Process_System_Time();
-    //pillbox1.Activate_pillBox();
-    (pillbox1->* & PillBox::Activate_pillBox)();
-    Drawer* d = (ptr_pillbox1->* & PillBox::Process_System_Time)();
-
-
-  
+    (pillbox1->*&PillBox::addDrawers)(std::move(drawer1));
+    (pillbox1->*&PillBox::addDrawers)(std::move(drawer2));
+    (pillbox1->*&PillBox::Activate_pillBox)();
+     Drawer* d = (ptr_pillbox1->*&PillBox::Process_System_Time)(3,50);
+    (pillbox1->*&PillBox::Switch_ON)(std::move(d),0);
+    (pillbox1->*&PillBox::Blink)(std::move(d));
+    (ptr_pillbox1->*&PillBox::Switch_OFF)(std::move(d));
     return 0;
-
 }
