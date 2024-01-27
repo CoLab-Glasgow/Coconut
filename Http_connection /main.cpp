2b3fde6cd469f40d706905d8505a8150ec3801d8
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-01-27 17:14:58 +0000

    Update main.cpp

--- Http_connection /main.cpp
+++ Http_connection /main.cpp
@@ -1,1 +1,13 @@
+#include<iostream>
+#include "HTTPConnectionBuilder.h" 
+int main() {
+    HTTPBuilder connection_builder;
 
+    // Calling the member functions on the object using the pointer-to-member-function syntax
+    (connection_builder->*&HTTPConnectionBuilder::add_header)("Content-Type: application/json");
+    (connection_builder->*&HTTPConnectionBuilder::add_header)("Authorization: Bearer YOUR_TOKEN_HERE");
+    (connection_builder->*&HTTPConnectionBuilder::add_body)("{ \"name\": \"John Doe\", \"age\": 30 }");
+    (connection_builder->*&HTTPConnectionBuilder::build)();
+
+    return 0;
+}
