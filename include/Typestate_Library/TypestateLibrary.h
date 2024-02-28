9317bbcaee954915e05c82726d685af6460f504a
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-02-28 14:29:03 +0000

    Update TypestateLibrary.h

--- include/Typestate_Library/TypestateLibrary.h
+++ include/Typestate_Library/TypestateLibrary.h
@@ -1,27 +1,25 @@
 #pragma once
 #include <iostream>
+#include "enum.h" 
+#include <functional>
+#include <map>
+#include <assert.h>
+#include <stdexcept>
 #include <fstream>
 #include <sstream>
 #include "enum.h"
-
 #include <functional>
 #include <map>
-#include <unordered_set>
-#include <functional>
 #include <assert.h>
 #include <utility>
 #include <stdexcept>
-#include <boost/hana/assert.hpp>
-#include <boost/hana/config.hpp>
 #include <boost/hana/equal.hpp>
 #include <boost/hana/fold_right.hpp>
 #include <boost/hana/if.hpp>
 #include <boost/hana/less.hpp>
-#include <boost/hana/prepend.hpp>
-#include <boost/static_assert.hpp>
+#include <boost/hana/fold.hpp>
 #include <boost/hana/tuple.hpp>
 #include <boost/hana/transform.hpp>
-#include <boost/hana/detail/index_if.hpp>
 #include <boost/hana/assert.hpp>
 #include <boost/hana/find.hpp>
 #include <boost/hana/back.hpp>
@@ -38,349 +36,265 @@
 #include <boost/type_index.hpp>
 
 
-
-
 namespace hana = boost::hana;
-using namespace hana::literals;
 
+
+#define ANSI_COLOR_RED "\x1b[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+
+
+
+// Handles errors by printing a message and exiting the program.
+#define HANDLE_ERROR(msg) do { \
+    std::cerr << ANSI_COLOR_RED << "Error: " << msg << ANSI_COLOR_RESET << std::endl; \
+    std::exit(EXIT_FAILURE); \
+} while (0)
 
 namespace TypestateLibrary {
 
+    /*
+    Struct `State` represents a single state within the typestate pattern. It encapsulates
+    a state, a function pointer as an action, and the next possible state. This struct
+    is crucial for peresent each valid transition made between
+    valid states as per the typestate specification.
+    */
+
     template<auto state, auto FP, auto NextState>
     struct State {
-        constexpr auto GetvalueState() {
-            constexpr auto valueState = state;
-            return valueState;
+        using StateType = decltype(state);
+        using FPType = decltype(FP);
+        using NextStateType = decltype(NextState);
 
+        // Validates the function pointer against the stored function pointer.
+        template<auto f>
+        constexpr bool check_value() {
+            return reinterpret_cast<decltype(FP)>(f) == FP;
         }
 
-        constexpr auto GetvalueFP() {
-            constexpr auto valueFP = FP;
-            return valueFP;
+        // Retrieves the current state.
+        constexpr auto GetValueState() {
+            return state;
         }
 
-        constexpr auto GetvalueNextState() {
-            constexpr auto valueNextState = NextState;
-            return valueNextState;
-
+        // Retrieves the function pointer associated with the current state.
+        constexpr auto GetValueFP() {
+            return FP;
         }
+
+        // Retrieves the next state following the current one.
+        constexpr auto GetValueNextState() {
+            return NextState;
+        }
+
+        // Converts state to string representation. 
         template <typename T>
-        std::string  printState(T s) {
-            std::string StateString = s._to_string();
-           return StateString;
+        std::string GetStateName(T s) {
+            return s._to_string();
         }
 
+        // Prints the current state to a string.
+        template <typename T>
+        std::string PrintState(T s) {
+            return s._to_string();
+        }
     };
 
 
+    /*
+    Struct `Typestate_Template` manages a collection of `State` instances, defining
+    the entire sequence of states and transitions for a typestate pattern. It supports
+    compile-time checks to ensure correct transitions and provides utilities for
+    accessing state information and visualizing the state machine.
+    */
 
-    template <typename... State>
+    template <typename... States>
     struct Typestate_Template {
+        hana::tuple<States...> states;
 
-        hana::tuple<State...> states;
-
-
-        auto Get_start_state() {
-            auto first_element = boost::hana::front(states);
-        
-        
-            return first_element.GetvalueState();
-
-        }
-        auto Get_last_state() {
-            auto last_element = boost::hana::back(states);
-            return last_element;
+        // Retrieves the starting state of the typestate sequence.
+        auto GetStartState() {
+            return hana::front(states).GetValueState();
         }
 
-        template <typename T >
-        void check_values(T state) {
-            constexpr auto b = state.GetvalueFP();
-         
+        // Retrieves the last state in the typestate sequence.
+        auto GetLastState() {
+            return hana::back(states);
         }
 
+        // Prints values associated with a given state, aiding in debugging and visualization.
         template <typename T>
-        void print_values(T state) {
-            std::cout << "state= " << state.GetvalueState()
-                << ", FunctionPointer= " << state.GetvalueFP()
-                << ", NextState= " << state.GetvalueNextState() << "\n";
-
+        void PrintValues(T state) {
+            std::cout << "State= " << state.GetValueState()
+                      << ", FunctionPointer= " << state.GetValueFP()
+                      << ", NextState= " << state.GetValueNextState() << std::endl;
         }
 
-        template<int x>
-        auto GetAState() {
-            return std::get<x>(states);
-        }
-
-        template <std::size_t... Is>
-        auto iterate_states(std::index_sequence<Is...>) {
-
-            (print_values(std::get<Is>(states)), ...);
-
-        }
-
-        void print_all_values() {
-
+        // Iterates through and prints all states and their associated values.
+        void PrintAllValues() {
             hana::for_each(states, [&](auto x) {
-                std::cout << "state= " << x.GetvalueState()
-                    << ", FunctionPointer= " << x.GetvalueFP()
-                    << ", NextState= " << x.GetvalueNextState() << "\n";
-                    
-                });
+                PrintValues(x);
+            });
         }
 
-
-        auto Get_target_state(auto b ,auto f) {
-
+        // Determines the target state based on a given state and function pointer.
+        auto Get_target_state(auto b, auto f) {
             auto first_element = boost::hana::front(states);
             auto result = hana::make_tuple();
-            auto y = first_element.GetvalueState();
-            auto z = first_element.GetvalueFP();
-            auto w = first_element.GetvalueNextState();
+            auto y = first_element.GetValueState();
+            auto z = first_element.GetValueFP();
+            auto w = first_element.GetValueNextState();
 
             hana::for_each(states, [&](auto x) {
-                
-                if (b == x.GetvalueState() && reinterpret_cast<decltype(x.GetvalueFP())>(f) == x.GetvalueFP() ) {
-                   
-                       std::cout << x.GetvalueState() << std::endl;
-                        y = x.GetvalueState();
-                        z = reinterpret_cast<decltype(z)>(x.GetvalueFP());
-                        w = x.GetvalueNextState();
-
-     
+                if (b == x.GetValueState() && reinterpret_cast<decltype(x.GetValueFP())>(f) == x.GetValueFP()) {
+                    std::cout << x.GetValueState() << std::endl;
+                    y = x.GetValueState();
+                    z = reinterpret_cast<decltype(z)>(x.GetValueFP());
+                    w = x.GetValueNextState();
                 }
-                });
+            });
 
             return hana::make_tuple(y, z, w);
         }
 
-
+        // Identifies if a method can be called at any time within the typestate sequence.
         auto find_any_time_method(auto m) {
             bool found = false;
             hana::for_each(states, [&](auto x) {
-                if (reinterpret_cast<decltype(x.GetvalueFP())>(m) == x.GetvalueFP()) {
+                if (reinterpret_cast<decltype(x.GetValueFP())>(m) == x.GetValueFP()) {
                     found = true;
                 }
-                });
+            });
             if (!found) {
                 auto any_time_method = m;
-
                 return any_time_method;
             }
-
         }
 
-
-        template<typename T>
+        // Generates a graph representation of the state transitions for visualization.
         void Visualize_TypestateTemplate() {
-
-            
-            hana::for_each(states, [&](auto state) {
-                auto x = state.GetvalueState();
-                
-                });
-
             std::ofstream file("state_machine.dot");
             file << "digraph G {" << std::endl;
 
             hana::for_each(states, [&](auto state) {
-                auto x = state.GetvalueState();
-                file << "    " << static_cast<int>(x) << " [label=\"" << state.printState<T>(x) << "\"];" << std::endl;
-            
-                });
-            auto last = Get_last_state();
-           file << "    " << static_cast<int>(last.GetvalueNextState()) << " [label=\"" << last.printState<T>(last.GetvalueNextState()) << "\"];" << std::endl;
+                auto x = state.GetValueState();
+                file << "    " << static_cast<int>(x) << " [label=\"" << state.PrintState(x) << "\"];" << std::endl;
+            });
 
+            auto last = GetLastState();
+            file << "    " << static_cast<int>(last.GetValueNextState()) << " [label=\"" << last.PrintState(last.GetValueNextState()) << "\"];" << std::endl;
 
-         
             hana::for_each(states, [&](auto state) {
-           
-            auto event = state.GetvalueFP();
-            std::string func_type_name = typeid(event).name();
-            std::string func_name = func_type_name.substr(func_type_name.find("(__cdecl") + 9);
+                auto fromState = state.GetValueState();
+                auto toState = state.GetValueNextState();
+                file << "    " << static_cast<int>(fromState) << " -> " << static_cast<int>(toState)
+                     << " [label=\"Transition\"];" << std::endl; // Simplified to "Transition"
+            });
 
-            
-           //func_name = func_name.substr(func_name.find("__ptr64)")+4);
-            auto fromState = state.GetvalueState();
-            auto toState =  state.GetvalueNextState();
-            file << "    " << static_cast<int>(fromState) << " -> " << static_cast<int>(toState)
-                << " [label=\"" << func_name << "\"];" <<
-                std::endl;    
-                
-                });
-
-            // Close the Graphviz file
             file << "}" << std::endl;
             file.close();
-            // Convert the filename to a wide-character string
-            int len = strlen("Typestates.png") + 1;
-            WCHAR* wstr = new WCHAR[len];
-            MultiByteToWideChar(CP_UTF8, 0, "Typestates.png", -1, wstr, len);
-
-            // Use the Graphviz command-line tool to convert the .dot file to an image
-            system("dot -Tpng state_machine.dot -o Typestates.png");
-
-            // Open the image in the default image viewer
-            ShellExecute(NULL, L"open", wstr, NULL, NULL, SW_SHOWNORMAL);
-
-            // Free the wide-character string
-            delete[] wstr;
-
-
         }
-
 
     };
-    
-    
-    
-    
 
     
+    /*
+    Class `State_Manager` enforces the typestate pattern while leveraging
+    compile-time defined states and transitions. It tracks instances of objects, ensuring
+    they move through their defined states according to the typestate specification.
+    */
 
-   
-    template <typename T, typename  Typestate_Template>
-    class Tracked : public T {
+
+    template <typename T, typename TypestateTemplate>
+    class State_Manager : public T {
     protected:
-        // static instances map that holds pointers to all instances of classes 
-        //that inherit from Tracked class
-        
-        Tracked() {
+        static int counter_;
+        static std::map<T*, int> TrackedInstances_;
+        TypestateTemplate Prot;
+        int V = Prot.GetStartState();
+
+    public:
+        State_Manager() {
             counter_++;
             TrackedInstances_.insert(std::make_pair(reinterpret_cast<T*>(this), V));
-           // std::cout << "Creating instance " << this << " at state " << V << std::endl;
-
+            std::cout << "Creating instance " << this << " at state " << V << std::endl;
         }
 
-        ~Tracked() {
+        ~State_Manager() {
             TrackedInstances_.erase(reinterpret_cast<T*>(this));
-           // std::cout << "Destroying instance " << this << " at state " << V << std::endl;
-        }
-    protected:
-        template <typename... Args>
-        auto Typestate_Checking(void (T::* func)(Args...), Args... args) -> decltype((std::declval<T>().*func)(args...)) {
-            std::cout << "Calling method " << typeid(func).name() << " on instance " << this << std::endl;
-            return (static_cast<T*>(this)->*func)(args...);
+            std::cout << "Destroying instance " << this << " at state " << V << std::endl;
         }
 
-        static std::map<T*, int>& TrackedInstances() {
-
+        // Static method to access all tracked instances.
+        static std::map<T*, int>& GetTrackedInstances() {
             return TrackedInstances_;
         }
 
-    private:
-
-        static int counter_;
-        static std::map<T*, int> TrackedInstances_;
-
-        Typestate_Template Prot;
-        int V = Prot.Get_start_state();
-
-    };
-
-    template<typename T, typename  Typestate_Template>
-    int Tracked<T, Typestate_Template>::counter_ = 0;
-
-    template<typename T, typename  Typestate_Template>
-    std::map< T*, int> Tracked<T, Typestate_Template>::TrackedInstances_;
-
-   
-
-    // The is_tracked trait is used to determine whether a class inherits from Tracked.
-    template <typename T, typename  Typestate_Template>
-    struct is_tracked : std::integral_constant<bool, std::is_base_of<Tracked<T, Typestate_Template>, T>::value> {};
-
-    template <typename T, typename P, typename = void>
-    class TypestateClassConnector : public T {};
-
-   
-    template <typename T, typename  Typestate_Template>
-    class TypestateClassConnector<T, Typestate_Template> : public Tracked<T, Typestate_Template> {
-
-
-
-
-    private:
-        T obj;
-        Typestate_Template Prot;
-        int size = sizeof(Prot.states);
-        static int counter_1;
-
-
-      
-
+        // Performs typestate checking before invoking a method on an instance.
         template <typename F, typename... Args>
-       
         void Typestate_Checking(F func, Args... args) {
-            
             const auto AnyMethod = Prot.find_any_time_method(func);
-            if (reinterpret_cast<decltype(AnyMethod)>(func) == AnyMethod) {
 
-               // std::cout << "Calling anytime method " << typeid(func).name() << " on instance " << this << std::endl;
+            if (func == AnyMethod) {
+                std::cout << "Calling anytime method " << typeid(func).name() << " on instance " << this << std::endl;
+            } else {
+                static std::map<T*, int>& mapRef = this->GetTrackedInstances();
+                const auto current_state = Prot.Get_target_state(mapRef[this], func);
+                const auto cs = boost::hana::at_c<0>(current_state);
+                const auto FP = boost::hana::at_c<1>(current_state);
+                const auto NS = boost::hana::at_c<2>(current_state);
 
-            }
-            
-            else {
+                if (cs == mapRef[this] && reinterpret_cast<decltype(FP)>(func) == FP) {
+                    mapRef[this] = NS;
+                } else {
 
-                static std::map<T*, int>& mapRef = this->TrackedInstances();
-                // std::cout << " object " << this << " at state " << mapRef[this] << std::endl;
-                auto cuerrent_state = Prot.Get_target_state(mapRef[this], func);
-                auto defstate = boost::hana::front(Prot.states);
-
-                const auto cs = boost::hana::at_c<0>(cuerrent_state);
-                const auto FP = boost::hana::at_c<1>(cuerrent_state);
-                const  auto NS = boost::hana::at_c<2>(cuerrent_state);
-                const auto value = reinterpret_cast<decltype(FP)>(func);
-                const auto cuerr = mapRef[this];
-
-                
-                
-                if (cs == mapRef[this] && reinterpret_cast<decltype(FP)>(func) == FP)
-
-                {
-                    
-                    
-                        mapRef[this] = NS;
-
-                       
-                }
-
-                else {
-                  
+                    // Handle error for invalid state transition or function call.
                     static_assert(std::is_same<decltype(FP), decltype(value)>::value,
                         "Calling method Cause errors, Follow the typestate specfications!");
-                   
-              assert(FP==value);
-              
+                    std::cerr << "Error: Invalid state transition or function call." << std::endl;
+                    HANDLE_ERROR("State transition or function call does not match the allowed transitions.");
                 }
-            
-                
-
             }
-
         }
-
-
-        
-      
-     
-    public:
-        template<typename... Args>
-        constexpr auto operator->*(auto (T::* funcPtr)(Args...))  {
-
-            return [this, funcPtr](Args&&... args) -> decltype(auto)  {
-                
-                Typestate_Checking(funcPtr, std::forward<Args>(args)...);
-                return  (this->*funcPtr)(std::forward<Args>(args)...);
-            };
-        }
-
-
-      
-
-
-
-
     };
+
+
     
 
+
+    template<typename T, typename TypestateTemplate>
+    int State_Manager<T, TypestateTemplate>::counter_ = 0;
+
+    template<typename T, typename TypestateTemplate>
+    std::map<T*, int> State_Manager<T, TypestateTemplate>::TrackedInstances_;
+
+    /*
+    Struct `is_tracked` is a compile-time check to determine if a given type is managed
+    by a `State_Manager`, ensuring it adheres to a typestate pattern. This facilitates
+    compile-time validation of the object lifecycle according to the typestate.
+    */
+
+    template <typename T, typename TypestateTemplate>
+    struct is_tracked : std::integral_constant<bool, std::is_base_of<State_Manager<T, TypestateTemplate>, T>::value> {};
+
+    /*
+    Class `TypestateClassConnector` integrates typestate functionality with user-defined
+    types. It intercepts method calls to perform typestate checks, ensuring that object
+    interactions adhere to the defined typestate pattern. 
+    */
+
+    template <typename T, typename TypestateTemplate>
+    class TypestateClassConnector : public State_Manager<T, TypestateTemplate> {
+    public: 
+       
+        template<typename... Args>
+        auto operator->*(auto (T::* funcPtr)(Args...)) {
+            return [this, funcPtr](Args&&... args) -> decltype(auto) {
+                this->Typestate_Checking(funcPtr, std::forward<Args>(args)...);
+                return (this->*funcPtr)(std::forward<Args>(args)...);
+            };
+        }
+    };
+
+    
 }
