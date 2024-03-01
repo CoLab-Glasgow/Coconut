66ceb5c04c54b4d2b09b435e9e298f7f75773c4f
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-03-01 15:33:43 +0000

    Polish test

--- PillBox/tests/Invalid_test2.cpp
+++ PillBox/tests/Invalid_test2.cpp
@@ -4,12 +4,9 @@
 int main() {
     Pillbox pillbox;
     Drawer* drawer = new Drawer("Vitamin C", 10, 500);
-
     (pillbox->*&PillBox::addDrawers)(std::move(drawer));
-
     (pillbox->*&PillBox::Activate_pillBox)();
     // Missing process time step
     (pillbox->*&PillBox::Switch_ON)(std::move(drawer),0); // This operation is invalid without processing time first.
-    
     return 0;
 }
