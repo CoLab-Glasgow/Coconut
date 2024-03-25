def22fb179bfdce888075323926c4296144dd5fd
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-03-25 00:20:01 +0000

    Update main.cpp

--- PillBox/main.cpp
+++ PillBox/main.cpp
@@ -1,8 +1,10 @@
-
-
 #include<iostream>
+#include <thread>
 #include "PillBox.h"
+
 int main() {
+    //PillBox_typestate pp; 
+    //pp.Visualize_TypestateTemplate();
     Drawer* drawer1 = new Drawer("Panadol", 3, 50);
     Drawer* drawer2 = new Drawer("Piriton Antihistamine", 8, 40);
     Pillbox pillbox1;
@@ -10,8 +12,9 @@ int main() {
     (pillbox1->*&PillBox::addDrawers)(std::move(drawer1));
     (pillbox1->*&PillBox::addDrawers)(std::move(drawer2));
     (pillbox1->*&PillBox::Activate_pillBox)();
-     Drawer* d = (ptr_pillbox1->*&PillBox::Process_System_Time)(3,50);
-    (pillbox1->*&PillBox::Switch_ON)(std::move(d),0);
+    Drawer* d = (pillbox1->*&PillBox::Process_System_Time)(3,50);
+    ///(pillbox1->*&PillBox::Blink)(std::move(d));
+    (ptr_pillbox1->*&PillBox::Switch_ON)(std::move(d),0);
     (pillbox1->*&PillBox::Blink)(std::move(d));
     (ptr_pillbox1->*&PillBox::Switch_OFF)(std::move(d));
     return 0;
