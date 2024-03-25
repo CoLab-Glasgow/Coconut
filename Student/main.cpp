7c604063a2b85d7633eeab7f5a3da0b05e4ceae5
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-03-25 00:22:47 +0000

    Update main.cpp

--- Student/main.cpp
+++ Student/main.cpp
@@ -4,6 +4,6 @@ int main(int argc, char** argv) {
     student s;
     (s->*&Student::Register)("Arwa","2598556A");
     (s->*&Student::Enrol)("Arwa");
-    (s->*&Student::Register)("Arwa", "2598556");
+    (s->*&Student::LogOut)("Arwa");
     return 0;
 };
