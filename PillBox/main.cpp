72b6fc7ca2ea89500b0470cb660fbe0a8fd4f202
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-04-12 13:46:56 +0000

    Update main.cpp

--- PillBox/main.cpp
+++ PillBox/main.cpp
@@ -9,13 +9,16 @@ int main() {
     Drawer* drawer2 = new Drawer("Piriton Antihistamine", 8, 40);
     Pillbox pillbox1;
     Pillbox& ptr_pillbox1 = pillbox1;
-    (pillbox1->*&PillBox::addDrawers)(std::move(drawer1));
-    (pillbox1->*&PillBox::addDrawers)(std::move(drawer2));
+    (pillbox1->*&PillBox::addDrawers)(drawer1);
+    (pillbox1->*&PillBox::addDrawers)(drawer1);
     (pillbox1->*&PillBox::Activate_pillBox)();
     Drawer* d = (pillbox1->*&PillBox::Process_System_Time)(3,50);
-    ///(pillbox1->*&PillBox::Blink)(std::move(d));
-    (ptr_pillbox1->*&PillBox::Switch_ON)(std::move(d),0);
-    (pillbox1->*&PillBox::Blink)(std::move(d));
-    (ptr_pillbox1->*&PillBox::Switch_OFF)(std::move(d));
+    int i=0;
+    while (i<5){
+    (ptr_pillbox1->*&PillBox::Switch_ON)(d);
+     i++;
+    }
+    (pillbox1->*&PillBox::Blink)(d);
+    (ptr_pillbox1->*&PillBox::Switch_OFF)(d);
     return 0;
 }
