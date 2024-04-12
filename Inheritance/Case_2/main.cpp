3d64069f488c9b28b41ea96f534bc4e5a47ccb73
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-04-12 14:16:08 +0100

    Update main.cpp

--- Inheritance/Case_2/main.cpp
+++ Inheritance/Case_2/main.cpp
@@ -1,25 +1,17 @@
- 
  #include "Vehicle.h"
  #include "Car.h"
- int main() {
-	 
+ int main() { 
 	 vehicle V1; 
 	 Car C;
 	 // will compile it follows the typestates
-	 (V1->* & Vehicle::Set_Speed)(70);
-	 (V1->* &Vehicle::Set_Speed)(50);
-         (V1->* &Vehicle::Stop)();
-        // will not compile
-       //(V1->* &Vehicle::SetSpeed)(70);
-      // the subclass also adhere to the typestates
-        (C->*&Vehicle::Set_Speed)(70);
-       (C->*&Car::Adjust_Seats)();
-       // this is an example of subclass that does not follow the typestate
-	 // it won't compile
-	 //(C->*&Car::StopTheVehicle)();
-	 //(C->*&Car::SetSpeed)(70);
-	
-
-
+	 (V1->*&Vehicle::Start)();
+	 (V1->*&Vehicle::Set_Speed)(70);
+	 (V1->*&Vehicle::Set_Speed)(50);
+         (V1->*&Vehicle::Stop)();
+       
+         (C->*&Vehicle::Start)();
+         (C->*&Vehicle::Set_Speed)(70);
+	 (C->*&Vehicle::Set_Speed)(30);
+	 (C->*&Vehicle::Stop)();
 	 return 0;
  }
