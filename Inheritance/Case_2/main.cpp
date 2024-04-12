f0dedc2d52b631ccb5ab4a844de12284a77eeb26
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-04-12 14:40:01 +0100

    Update main.cpp

--- Inheritance/Case_2/main.cpp
+++ Inheritance/Case_2/main.cpp
@@ -1,17 +1,18 @@
  #include "Vehicle.h"
  #include "Car.h"
- int main() { 
+ int main() {
 	 vehicle V1; 
 	 Car C;
-	 // will compile it follows the typestates
 	 (V1->*&Vehicle::Start)();
 	 (V1->*&Vehicle::Set_Speed)(70);
 	 (V1->*&Vehicle::Set_Speed)(50);
-         (V1->*&Vehicle::Stop)();
+     (V1->*&Vehicle::Stop)();
        
-         (C->*&Vehicle::Start)();
-         (C->*&Vehicle::Set_Speed)(70);
+     (C->*&Vehicle::Start)();
+     (C->*&Vehicle::Set_Speed)(70);
+	 (C->*&Car::Play_Music)();
 	 (C->*&Vehicle::Set_Speed)(30);
+	 (C->*&Car::Adjust_Seats)();
 	 (C->*&Vehicle::Stop)();
 	 return 0;
  }
