7b7c81f34dd50ac01946ac40818bd77eb6e6ce06
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-12-11 21:04:55 +0000

    edit Student

--- Student/Student.h
+++ Student/Student.h
@@ -1,41 +1,52 @@
 #include <cstdlib>
-#include<iostream>
-#include<string>
-#include "TypestateLibrary.h"
+#include <iostream>
+#include <string>
+#include "../include/Typestate_Library/TypestateLibrary.h"
 
-using TypestateLibrary::TypestateClassConnector;
-using TypestateLibrary::State;
-using TypestateLibrary::Typestate_Template;
+using TypestateLibrary::Template::TypestateClassConnector;
+using TypestateLibrary::Template::State;
+using TypestateLibrary::Template::Typestate_Template;
+
 
 class Student {
 public:
-    Student() { }
-    void Register(std::string n , std::string i) {
-        std::cout << n << " has been registered with ID" << i << std::endl;
+    
+    void Register (const char* n , const char* id) {
+       printf("Registering student: Name = %s, ID = %s\n", n, id);
     }
-    void Enrol(std::string n) {
-        std::cout << n << "has been enrolled in a course" << std::endl;
+    void Enrol(const char* n) {
+        printf("Enrolling student in: %s\n", n);
     }
-    void LogOut(std::string n) {
-        std::cout << n << "has Logged out" << std::endl;
+    void LogOut(const char* n) {
+       printf("Logging out student: %s\n", n);
     }    
+    
 
 };
 
 
-BETTER_ENUM (Studentstate, int, 
-    UNAPPROVED,
-    APPROVED,
-    ENROLLED,
-    END
-    )
+
+enum class Studentstate { 
+    UNAPPROVED =0,
+    APPROVED=1,
+    ENROLLED=2,
+    END=3
+};
 
 
 using StudentProtocol = Typestate_Template<
-    State<+Studentstate::UNAPPROVED, &Student::Register, +Studentstate::APPROVED>,
-    State<+Studentstate::APPROVED, &Student::Enrol, +Studentstate::ENROLLED>,
-    State<+Studentstate::ENROLLED, &Student::Enrol, +Studentstate::ENROLLED>,
-    State<+Studentstate::END, &Student::LogOut, +Studentstate::END>
+    State<Studentstate::UNAPPROVED, &Student::Register, Studentstate::APPROVED>,
+    State<Studentstate::APPROVED, &Student::Enrol, Studentstate::ENROLLED>,
+    State<Studentstate::ENROLLED, &Student::Enrol, Studentstate::ENROLLED>,
+    State<Studentstate::ENROLLED, &Student::LogOut, Studentstate::END>
 >;
 
-using student = TypestateClassConnector<Student, StudentProtocol>;
+using student_Flag = TypestateClassConnector<Student, StudentProtocol>;
+
+student_Flag StudentFlag;
+
+void init(){
+    StudentProtocol t;
+    t.display();
+    StudentFlag.display();
+}
--- Student/main.cpp
+++ Student/main.cpp
@@ -1,9 +1,12 @@
 #include "Student.h"
 
 int main(int argc, char** argv) {   
-    student s;
-    (s->*&Student::Register)("Arwa","2598556A");
-    (s->*&Student::Enrol)("Arwa");
-    (s->*&Student::LogOut)("Arwa");
+    Student s;
+    Student& s2=s;
+    
+    s.Register("ARWA","38940");
+    s.Enrol("ARWA");
+    s2.LogOut("Arwa");
+
     return 0;
 };
