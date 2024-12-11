b68b6cbb7ed50644213e66f70488a93b0c29cdd9
ArwaAlsubhiM <94806060+ArwaAlsubhiM@users.noreply.github.com>
2024-12-11 20:21:08 +0000

    Add V2

---
+++ include/Typestate_Library/V1/Typestate_Library/TypestateLibrary.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <iostream>
+#include <functional>
+#include <map>
+#include <cassert>
+#include <stdexcept>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+#include <mutex>
+#include <utility>
+#include <optional>
+#include <set>
+#include <functional>
+#include <string>
+#include <tuple>      
+#include <algorithm>  
+#include <type_traits>
+
+using namespace std::chrono_literals;
+
+namespace TypestateLibrary {
+
+    namespace Template {
+
+        template<auto state, auto FP, auto NextState>
+        struct State{
+            void display() {}
+        };
+
+
+        template<typename TFirst, typename... TRest>
+        struct Typestate_Template{
+        void display() {
+        TFirst t;
+        t.display();
+        if constexpr (sizeof...(TRest) > 0) { // Ensure no instantiation when empty
+            Typestate_Template<TRest...> rest;
+            rest.display();
+        }
+    }
+   };
+        
+
+        template<typename T, typename Typestate>
+        struct TypestateClassConnector {
+            void display() {}
+          };
+
+    }
+
+
+    
+    
+    }
